5bec9bb17b235df737f4f6468eaf6cda0eaa2e75
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-01-27 12:54:54 -0600

    added additional tests

--- 02week/tests.js
+++ 02week/tests.js
@@ -11,12 +11,20 @@ if (typeof describe === 'function') {
     it('should detect which hand won', () => {
       assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
       assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.equal(rockPaperScissors('scissors', 'rock'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+      assert.equal(rockPaperScissors('paper', 'rock'), "Hand one wins!");
+      assert.equal(rockPaperScissors('scissors', 'paper'), "Hand one wins!");
     });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
       assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
       assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+    });
+    it('should check to see if inputs are valid', () => {
+      assert.equal(rockPaperScissors('leaf', 'river'), 'Invalid input! Please try again and type either rock, paper or scissors');
+      assert.equal(rockPaperScissors('rck', 'apaper'), 'Invalid input! Please try again and type either rock, paper or scissors');
+      assert.equal(rockPaperScissors('p', 's'), 'Invalid input! Please try again and type either rock, paper or scissors');
     });
   });
 } else {
