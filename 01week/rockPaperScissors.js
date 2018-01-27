f95532a17cb5c81223db147d620b14537b599719
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-01-27 13:35:52 -0600

    combined functions into one and cleaned code

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -2,60 +2,56 @@
 
 const assert = require('assert');
 const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
-const rockPaperScissors=(hand1, hand2)=> {
-  // SOLUTION #1:
-  // solution using if/else statements to take care of all 9 possible cases
-  if (hand1 === hand2){  // first if takes care of 3 cases of tie
-    return "It's a tie!"
-  } else if ((hand1 === 'rock' && hand2 === 'scissors') || //second if takes care of 3 cases where hand1 wins
-            (hand1 === 'paper' && hand2 === 'rock') ||
-            (hand1 === 'scissors' && hand2 === 'paper')){
-    return "Hand one wins!"
-  } else {   //else takes care of last 3 cases where hand2 wins
-    return "Hand two wins!"
-  };
+const rockPaperScissors = (hand1, hand2) => {
 
+  const checkForWin = (hand1, hand2) => {
+    // SOLUTION #1:
+    // solution using if/else statements to take care of all 9 possible cases
+    if (hand1 === hand2) { // first if takes care of 3 cases of tie
+      return "It's a tie!"
+    } else if ((hand1 === 'rock' && hand2 === 'scissors') || //second if takes care of 3 cases where hand1 wins
+    (hand1 === 'paper' && hand2 === 'rock') || (hand1 === 'scissors' && hand2 === 'paper')) {
+      return "Hand one wins!"
+    } else { //else takes care of last 3 cases where hand2 wins
+      return "Hand two wins!"
+    };
 
-  // //SOLUTION #2:
-  // // alternate solution using if and switch statements that takes care of all 9 possible cases
-  // if(hand1 === hand2) return 'The result is a tie!'; //checks for tie result first, eliminates 3 cases
-  //
-  // switch(hand1+hand2){  //switch takes input of both strings and concatnates them into single string which can then be tested for truthiness
-  // case 'rockscissors':  //3 more cases eliminated when hand1 the winner
-  // case 'paperrock':
-  // case 'scissorspaper':
-  //   return "Hand one wins!"
-  //   break;
-  // default:  //remaining 3 cases taken care when hand2 the winner
-  //   return "Hand two wins!"
-  // };
-
-}
-
-const isUserInputValid=(hand1, hand2)=> {
-  const possibleInputs = ['rock', 'paper', 'scissors']
-  const newHand1 = hand1.toLowerCase().trim()
-  const newHand2 = hand2.toLowerCase().trim()
-
-  if((possibleInputs.indexOf(newHand1) !== -1) ||
-    (possibleInputs.indexOf(newHand2) !== -1) ){
-    return rockPaperScissors(hand1, hand2);
-  } else {
-    return 'Invalid input! Please try again and type either rock, paper or scissors'
+    // SOLUTION #2:
+    //  alternate solution using if and switch statements that takes care of all 9 possible cases
+    // if(hand1 === hand2) return 'The result is a tie!'; checks for tie result first, eliminates 3 cases
+    //
+    // switch(hand1+hand2){  switch takes input of both strings and concatnates them into single string which can then be tested for truthiness
+    // case 'rockscissors':  3 more cases eliminated when hand1 the winner
+    // case 'paperrock':
+    // case 'scissorspaper':
+    //   return "Hand one wins!"
+    //   break;
+    // default:  remaining 3 cases taken care when hand2 the winner
+    //   return "Hand two wins!"
+    // };
   }
+
+  const isUserInputValid = (hand1, hand2) => {
+    const possibleInputs = ['rock', 'paper', 'scissors']
+    const newHand1 = hand1.toLowerCase().trim()
+    const newHand2 = hand2.toLowerCase().trim()
+
+    if ((possibleInputs.indexOf(newHand1) !== -1) || (possibleInputs.indexOf(newHand2) !== -1)) {
+      return checkForWin(hand1, hand2);
+    } else {
+      return 'Invalid input! Please try again and type either rock, paper or scissors'
+    }
+  }
+
+  return isUserInputValid(hand1, hand2);
 }
-
-
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
-      console.log( isUserInputValid(answer1, answer2));
+      console.log(rockPaperScissors(answer1, answer2));
       getPrompt();
     });
   });
