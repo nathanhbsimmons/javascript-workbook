b5f4b8b7a495fb64ac59ae2139c37b8025aae268
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-01-30 18:34:17 -0600

    added tests

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -5,6 +5,7 @@ const readline = require('readline');
 const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
 const rockPaperScissors = (hand1, hand2) => {
+  
 
   const checkForWin = (hand1, hand2) => {
     // SOLUTION #1:
@@ -39,7 +40,7 @@ const rockPaperScissors = (hand1, hand2) => {
     const newHand2 = hand2.toLowerCase().trim()
 
     if ((possibleInputs.indexOf(newHand1) !== -1) || (possibleInputs.indexOf(newHand2) !== -1)) {
-      return checkForWin(hand1, hand2);
+      return checkForWin(newHand1, newHand2);
     } else {
       return 'Invalid input! Please try again and type either rock, paper or scissors'
     }
@@ -70,12 +71,20 @@ if (typeof describe === 'function') {
     it('should detect which hand won', () => {
       assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
       assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.equal(rockPaperScissors('scissors', 'rock'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+      assert.equal(rockPaperScissors('paper', 'rock'), "Hand one wins!");
+      assert.equal(rockPaperScissors('scissors', 'paper'), "Hand one wins!");
     });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
       assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
       assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+      assert.equal(rockPaperScissors('ScissoRS ', 'pape r'), "Hand one wins!");
+    });
+    it('should check to see if inputs are valid', () => {
+      assert.equal(rockPaperScissors('leaf', 'river'), 'Invalid input! Please try again and type either rock, paper or scissors');
+      assert.equal(rockPaperScissors('rck', 'apaper'), 'Invalid input! Please try again and type either rock, paper or scissors');
+      assert.equal(rockPaperScissors('p', 's'), 'Invalid input! Please try again and type either rock, paper or scissors');
     });
   });
 } else {
