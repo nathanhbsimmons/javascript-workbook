1044ecdddeaadadc8ad71ac00d54236e6f1e5283
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-02-06 18:38:26 -0600

    working on refactoring win checks

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -17,9 +17,9 @@ let playerTurn = 'X';
 
 //each row on the board is a seperate array. Each of these arrays are set to a
 //different variable to more easily mainpulate them
-let rowZero = board[0]
-let rowOne = board[1]
-let rowTwo = board[2]
+// let rowZero = board[0]
+// let rowOne = board[1]
+// let rowTwo = board[2]
 
 function printBoard() {
   console.log('   0  1  2');
@@ -31,29 +31,34 @@ function printBoard() {
 };
 
 function horizontalWin() {
-//checks for the 3 cases of a horizontal win
-  if ((rowZero[0] == playerTurn && rowZero[1] == playerTurn && rowZero[2] == playerTurn) ||
-    (rowOne[0] == playerTurn && rowOne[1] == playerTurn && rowOne[2] == playerTurn) ||
-    (rowTwo[0] == playerTurn && rowTwo[1] == playerTurn && rowTwo[2] == playerTurn)){
+  //checks for the 3 cases of a horizontal win
+  if ((board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') ||
+    (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') ||
+    (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X' || board[0] == 'O' && board[1] == 'O' && board[2] == 'O') ||
+      (board[0] == 'O' && board[1] == 'O' && board[2] == 'O') ||
+      (board[0] == 'O' && board[1] == 'O' && board[2] == 'O')){
     console.log(`${playerTurn} wins!`)
+    return true
   }
 
 };
 
 function verticalWin() {
-//checks for the 3 cases of a vert win
-  if ((rowZero[0] === playerTurn && rowOne[0] === playerTurn && rowTwo[0] === playerTurn) ||
+  //checks for the 3 cases of a vert win
+  if ((rowZero[0] === 'X' && rowOne[0] === 'X' && rowTwo[0] === playerTurn) ||
     (rowZero[1] === playerTurn && rowOne[1] === playerTurn && rowTwo[1] === playerTurn) ||
     (rowZero[2] === playerTurn && rowOne[2] === playerTurn && rowTwo[2] === playerTurn)) {
     console.log(`${playerTurn} wins!`)
+    return true
   }
 };
 
 function diagonalWin() {
-//checks for last 2 cases of win, diagonally
+  //checks for last 2 cases of win, diagonally
   if ((rowZero[0] === playerTurn && rowOne[1] === playerTurn && rowTwo[2] === playerTurn) ||
     (rowZero[2] === playerTurn && rowOne[1] === playerTurn && rowTwo[0] === playerTurn)) {
     console.log(`${playerTurn} wins!`)
+    return true
   }
 };
 
@@ -65,11 +70,11 @@ function diagonalWin() {
 //   });
 // }
 
-function checkForWin() {
+function checkForWin(board) {
   //8 winning cases are checked with these 3 seperate functions
-  horizontalWin();
-  verticalWin();
-  diagonalWin();
+  horizontalWin(board);
+  // verticalWin();
+  // diagonalWin();
   // checkForTie();
 
 };
@@ -86,15 +91,16 @@ function switchPlayerTurn() {
 function ticTacToe(row, column) {
   //using splice() to delete index (indicated by column input) in
   //array (indicated by row input)  and replacing with playerTurn
+
   if (row == 0) {
-    rowZero.splice(column, 1, playerTurn)
+    board[0][column] = playerTurn
   } else if (row == 1){
-    rowOne.splice(column, 1, playerTurn)
+    board[1][column] = playerTurn
   } else if (row == 2){
-    rowTwo.splice(column, 1, playerTurn)
+    board[2][column] = playerTurn
   }
 
-  checkForWin();
+  checkForWin(board);
   switchPlayerTurn();//switches player turn ONLY after board is set and win is checked for
 
 };
@@ -128,18 +134,18 @@ if (typeof describe === 'function') {
     });
     it('should check for vertical wins', () => {
       board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
-      assert.equal(verticalWin(), console.log(`${playerTurn} wins!`));
+      assert.equal(verticalWin(), true);
     });
     it('should check for horizontal wins', () => {
       board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
-      assert.equal(horizontalWin(), console.log(`${playerTurn} wins!`));
+      assert.equal(horizontalWin(), true);
     });
     it('should check for diagonal wins', () => {
       board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
-      assert.equal(diagonalWin(),  console.log(`${playerTurn} wins!`));
+      assert.equal(diagonalWin(),  true);
     });
     it('should detect a win', () => {
-      assert.equal(checkForWin(), console.log(`${playerTurn} wins!`));
+      assert.equal(checkForWin(), true);
     });
   });
 } else {
