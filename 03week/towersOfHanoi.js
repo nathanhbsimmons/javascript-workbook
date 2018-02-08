f1f675679cd1dd2af2eaaa08a70d1b2a4f256aa4
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-02-08 11:18:23 -0600

    added tests

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -30,10 +30,10 @@ function movePiece(startStack, endStack) {
 function isLegal(startStack, endStack) {
   if (stacks[startStack].length == 0){
     console.log("Can't start from an empty stack!")
-    return
+    return true
   } else if (stacks[startStack][stacks[startStack].length-1] > stacks[endStack][stacks[endStack].length-1]){
     console.log("Can't move larger number on top of smaller number")
-    return
+    return true
   } else {
     return movePiece(startStack, endStack);
   }
@@ -72,4 +72,54 @@ function getPrompt() {
   });
 }
 
-getPrompt();
+
+
+if (typeof describe === 'function') {
+
+  describe('#towersOfHanoi()', () => {
+    it('should move stacks', () => {
+      towersOfHanoi('a', 'b');
+      assert.deepEqual(stacks, {
+        a: [4, 3, 2],
+        b: [1],
+        c: []
+      })
+    });
+    it('should not allow illegal move', () => {
+      stacks = {
+        a: [4, 3, 2],
+        b: [1],
+        c: []
+      };
+      assert.equal(isLegal('a', 'b'), true);
+    });
+    it('should detect a win', () => {
+      stacks = {
+        a: [],
+        b: [4, 3, 2,1],
+        c: []
+      };
+      assert.equal(checkForWin(), reset());
+    });
+    it('should reset', () => {
+      stacks = {
+        a: [4, 3, 2],
+        b: [1],
+        c: []
+      };
+      reset();
+      assert.deepEqual(stacks, {
+        a: [4, 3, 2,1],
+        b: [],
+        c: []
+      })
+    });
+  });
+} else {
+
+  getPrompt();
+
+}
+
+
+// getPrompt();
